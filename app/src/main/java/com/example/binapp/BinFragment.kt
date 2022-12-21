03bc146f1e01a01e2eb15e6fdebb748055013c70
Fovteltar <nikitindima002@gmail.com>
2022-12-21 20:59:20 +0300

    Created BIN_VM. Added BIN data class and MutableDataFlow for BIN. Hardcoded BIN example.

--- app/src/main/java/com/example/binapp/BinFragment.kt
+++ app/src/main/java/com/example/binapp/BinFragment.kt
@@ -1,15 +1,21 @@
 package com.example.binapp
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.binapp.databinding.FragmentBinBinding
 
 class BinFragment: Fragment() {
     private var _binding: FragmentBinBinding? = null
     private val binding get() = _binding!!
+
+    private val binViewModel: BinViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,7 +29,35 @@ class BinFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // button hooks
+        binding.editTextBin.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun afterTextChanged(p0: Editable?) {}
+            override fun onTextChanged(string: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if (string?.length == BIN_AMOUNT) {
+                    Toast.makeText(context, "6 chars", Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
+
+        binViewModel.bin.observe(viewLifecycleOwner) {
+            val bin = binViewModel.bin.value
+            if (bin != null) {
+                binding.apply {
+                    textScheme.text = bin.scheme.replaceFirstChar{ it.uppercaseChar() }
+                    textType.text = bin.type.replaceFirstChar{ it.uppercaseChar() }
+                    textBank.text = bin.bank.name
+                    textLink.text = bin.bank.url
+                    textPhone.text = bin.bank.phone
+                    textBrand.text = bin.brand
+                    textPrepaid.text = if(bin.prepaid) "Yes" else "No"
+                    textLength.text = bin.number.length.toString()
+                    textLuhn.text = if(bin.number.luhn) "Yes" else "No"
+                    textCountry.text = bin.country.emoji + bin.country.name
+                    textLatitude.text = bin.country.latitude.toString()
+                    textLongitude.text = bin.country.longitude.toString()
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/example/binapp/BinViewModel.kt
@@ -0,0 +1,70 @@
+package com.example.binapp
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+const val BIN_AMOUNT = 6
+
+data class Number(
+    val length: Int,
+    val luhn: Boolean
+)
+
+data class Country(
+    val numeric: Int,
+    val alpha2: String,
+    val name: String,
+    val emoji: String,
+    val currency: String,
+    val latitude: Int,
+    val longitude: Int
+)
+
+data class Bank(
+    val name: String,
+    val url: String,
+    val phone: String
+)
+
+data class Bin(
+    val number: Number,
+    val scheme: String,
+    val type: String,
+    val brand: String,
+    val prepaid: Boolean,
+    val country: Country,
+    val bank: Bank
+)
+
+class BinViewModel: ViewModel() {
+    private var _bin: MutableLiveData<Bin> = MutableLiveData()
+    val bin: LiveData<Bin> get() = _bin
+
+    init {
+        _bin.value = Bin(
+            number = Number(
+                length = 16,
+                luhn = true
+            ),
+            scheme = "visa",
+            type = "credit",
+            brand = "Business",
+            prepaid = false,
+            country = Country(
+                numeric = 710,
+                alpha2 = "ZA",
+                name = "South Africa",
+                emoji = "🇿🇦",
+                currency = "ZAR",
+                latitude = -29,
+                longitude = 24
+            ),
+            bank = Bank(
+                name = "BIDVEST BANK",
+                url = "www.bidvestbank.co.za",
+                phone = "0860 11 11 77 OR 0860 102 499"
+            )
+        )
+    }
+}
